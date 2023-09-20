74bdac45f1997b11a3bbc0db445ccc0e56f2cfa4
tamo8 <141841268+tamo8@users.noreply.github.com>
2023-09-20 17:17:48 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+let age = Number (prompt("Enter your budget"))
+let message = age < 50 ? "less than 50" : "error"
+if (age > 20 ) console.log("more than 20") 
+console.log(message)
---
+++ second.js
@@ -0,0 +1,5 @@
+let a = "mariami"
+let bool = true;
+let bool2 = false;
+if (a = "mariami") {console.log("true");}
+else {console.log("false");}
